0b0c617def21d53c224bb382ef01308e0a2bafcd
qinpeng <ching9402@163.com>
2019-09-29 15:29:54 +0800

    project init

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,15 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
-
+import Home from '@/pages/home/Home'
+// import List from '@/pages/list/List'
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Home',
+      component: Home
     }
   ]
 })
