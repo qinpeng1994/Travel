091178b317d4efb6673be9f7a73a9f70919ff3db
qinpeng <ching9402@163.com>
2019-09-29 23:28:46 +0800

    banner change

--- src/main.js
+++ src/main.js
@@ -7,9 +7,12 @@ import 'styles/reset.css'
 import 'styles/border.css'
 import 'styles/iconfont.css'
 import fastclick from 'fastclick'
+import VueAwesomeSwiper from 'vue-awesome-swiper'
+import 'swiper/dist/css/swiper.css'
 
 Vue.config.productionTip = false
 fastclick.attach(document.body)
+Vue.use(VueAwesomeSwiper)
 
 /* eslint-disable no-new */
 new Vue({
---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="wrapper">
+        <swiper :options="swiperOption" >
+            <swiper-slide v-for="item of swiperList" :key="item.id">
+                <img  class="swiper-img" :src="item.imgUrl" />
+            </swiper-slide>
+            <div class="swiper-pagination"  slot="pagination"></div>
+        </swiper>
+    </div>
+</template>
+<script>
+export default {
+  name: 'HomeSwiper',
+    data (){
+        return{
+        swiperOption:{
+            pagination:'.swiper-pagination',
+            loop:true
+        },
+        swiperList:[
+            { id:'0001',imgUrl:"https://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20197/a685001bbf5e77a203ff8815e953efbe.jpg_945x288_52b6ec01.jpg"},
+            { id:'0002',imgUrl:"https://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20199/478f98476b86f665c958d01f39e14ace.jpg"}
+            ]
+        }
+        
+    }
+    
+}
+</script>
+
+<style lang="stylus" scoped>
+    .wrapper >>> .swiper-pagination-bullet-active
+        background:#fff
+    .wrapper
+        width:100%
+        height:0
+        overflow:hidden
+        padding-bottom:31.25%
+        background:#fff
+        .swiper-img
+            width:100%
+</style>
