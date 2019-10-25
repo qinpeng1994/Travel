59ba73ee104db8b161008a462fb9226cc1f5047b
qinpeng <ching9402@163.com>
2019-10-25 15:14:37 +0800

    finish csslist

---
+++ src/pages/city/components/Alphabet.vue
@@ -0,0 +1,33 @@
+<template>
+  <div >
+    <ul class="list">
+      <li class="item">A</li>
+      <li class="item">B</li>
+      <li class="item">C</li>
+      <li class="item">D</li>
+      <li class="item">E</li>
+    </ul>
+  </div>
+</template>
+<script>
+export default {
+  name: "City-Alphabet"
+};
+</script>
+<style lang="stylus" scoped>
+@import '~styles/varibles.styl'
+.list
+    position :absolute
+    top:1.58rem
+    right:0
+    bottom:0
+    width:.4rem
+    text-align :center
+    display:flex
+    flex-direction :column
+    justify-content :center
+    .item
+        line-height :.4rem
+        color :$bgColor
+   
+</style>
---
+++ src/pages/city/components/List.vue
@@ -0,0 +1,138 @@
+<template>
+  <div class="list" ref="wrapper">
+    <div>
+      <!-- 当前城市 -->
+      <div class="area">
+        <div class="title border-bottom">当前城市</div>
+        <div class="button-list">
+          <div class="button-wrapper">
+            <div class="button">北京</div>
+          </div>
+        </div>
+      </div>
+      <div class="area">
+        <!-- 热门城市 -->
+        <div class="title border-bottom">热门城市</div>
+        <div class="button-list">
+          <div class="button-wrapper">
+            <div class="button">北京</div>
+          </div>
+          <div class="button-wrapper">
+            <div class="button">北京</div>
+          </div>
+          <div class="button-wrapper">
+            <div class="button">北京</div>
+          </div>
+          <div class="button-wrapper">
+            <div class="button">北京</div>
+          </div>
+        </div>
+      </div>
+      <div class="area">
+        <!-- 城市首字母列表 -->
+        <div class="title border-topbottom">A</div>
+        <div class="item-list">
+          <div class="item border-bottom">阿拉尔</div>
+          <div class="item border-bottom">阿拉尔</div>
+          <div class="item border-bottom">阿拉尔</div>
+          <div class="item border-bottom">阿拉尔</div>
+          <div class="item border-bottom">阿拉尔</div>
+          <div class="item border-bottom">阿拉尔</div>
+        </div>
+        <div class="title border-topbottom">A</div>
+        <div class="item-list">
+          <div class="item border-bottom">阿拉尔</div>
+          <div class="item border-bottom">阿拉尔</div>
+          <div class="item border-bottom">阿拉尔</div>
+          <div class="item border-bottom">阿拉尔</div>
+          <div class="item border-bottom">阿拉尔</div>
+          <div class="item border-bottom">阿拉尔</div>
+        </div>
+        <div class="title border-topbottom">A</div>
+        <div class="item-list">
+          <div class="item border-bottom">阿拉尔</div>
+          <div class="item border-bottom">阿拉尔</div>
+          <div class="item border-bottom">阿拉尔</div>
+          <div class="item border-bottom">阿拉尔</div>
+          <div class="item border-bottom">阿拉尔</div>
+          <div class="item border-bottom">阿拉尔</div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import Bscroll from 'better-scroll'
+export default {
+  name: "CityList",
+  mounted(){
+      this.scroll = new Bscroll(this.$refs.wrapper)
+
+  }
+};
+</script>
+<style lang="stylus" scoped>
+@import '~styles/varibles.styl';
+
+.border-bottom {
+  &:after {
+    border-color: #ccc;
+  }
+
+  &:before {
+    border-color: #ccc;
+  }
+}
+
+.border-topbottom {
+  &:after {
+    border-color: #ccc;
+  }
+
+  &:before {
+    border-color: #ccc;
+  }
+}
+
+.list {
+  position: absolute;
+  left: 0;
+  right: 0;
+  top: 1.58rem;
+  bottom: 0;
+  overflow: hidden;
+
+  .title {
+    line-height: 0.6rem;
+    height: 0.6rem;
+    background: #eee;
+    padding-left: 0.2rem;
+    font-size: 0.26rem;
+  }
+
+  .button-list {
+    overflow: hidden;
+    padding: 0.1rem 0.6rem 0.1rem 0.1rem;
+
+    .button-wrapper {
+      width: 33.33%;
+      float: left;
+
+      .button {
+        margin: 0.1rem;
+        padding: 0.1rem 0;
+        text-align: center;
+        border: 0.02rem solid #ccc;
+        border-radius: 0.06rem;
+      }
+    }
+  }
+
+  .item-list {
+    .item {
+      line-height: 0.76rem;
+      padding-left: 0.2rem;
+    }
+  }
+}
+</style>
