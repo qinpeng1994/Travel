a90ea7335044ae982d96e20aa6bd9e75203a52bb
qinpeng <ching9402@163.com>
2019-10-25 10:46:51 +0800

    city-search

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -1,14 +1,18 @@
 <template>
     <div>
        <city-header/>
+       <city-search/>
     </div>
 </template>
 <script>
 import CityHeader from './components/Header'
+import CitySearch from './components/Search'
 export default {
     name:'City',
     components:{
-        CityHeader
+        CityHeader,
+        CitySearch
+
 
     }
 
---
+++ src/pages/city/components/Search.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="search">
+        <input type="text" placeholder="输入城市名或拼音" class="search-input">
+    </div>
+</template>
+<script>
+export default {
+    name: 'CitySearch'
+}
+</script>
+<style lang="stylus" scoped>
+ @import '~styles/varibles.styl'
+    .search
+        background :$bgColor
+        height :.72rem
+        padding: 0 .1rem
+        .search-input
+            height :.62rem
+            line-height :.62rem
+            width:100%
+            text-align:center
+            border-radius:.06rem
+            color:#666
+
+</style>
