669e780b44032b017c600d144a2c2bee12e7eba8
qinpeng <ching9402@163.com>
2019-10-25 10:32:36 +0800

    city router

---
+++ src/pages/city/components/Header.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+        <div class="header">
+            城市选择
+            <router-link to="/">
+                 <div class="iconfont header-icon">&#xe624;</div>
+            </router-link>
+           
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    name:"CityHeader"
+    
+}
+</script>
+<style lang="stylus" scoped>
+    @import '~styles/varibles.styl'
+    .header
+        position :relative
+        height :$headerHeight
+        line-height :$headerHeight
+        text-align:center
+        color:#fff
+        background :$bgColor
+        font-size:.32rem
+        .header-icon
+            position :absolute
+            left :0
+            top:0
+            width :.64rem
+            text-align :center
+            font-size:.4rem
+            color:#fff
+        
+
+
+</style>
--- src/pages/home/components/Header.vue
+++ src/pages/home/components/Header.vue
@@ -7,10 +7,13 @@
         <span class="iconfont">&#xe632;</span>
         输入城市/景点/游玩主题
         </div>
-        <div class="header-right">
-           {{city}}
-            <span class="iconfont arrow-icon">&#xe64a;</span>
-        </div>
+        <router-link to="/city">
+            <div class="header-right">
+            {{city}}
+                <span class="iconfont arrow-icon">&#xe64a;</span>
+            </div>
+        </router-link>
+        
     </div>
 </template>
 <script>
